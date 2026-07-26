f6caae32509cd1b11e5738f19f3c4480e47d73ad
AkulSharma <sharmaakul2007@gmail.com>
2026-07-27 00:04:48 +0530

    auto: Traffic.tsx

--- web-migration/components/Traffic.tsx
+++ web-migration/components/Traffic.tsx
@@ -5,6 +5,7 @@ import { useFrame } from "@react-three/fiber";
 import { RigidBody, type RapierRigidBody } from "@react-three/rapier";
 import * as THREE from "three";
 import { CarMesh } from "@/components/Car";
+import { RIDE_HEIGHT } from "@/components/SupercarBody";
 import { PoliceCarMesh } from "@/components/PoliceCar";
 import { useHudStore } from "@/lib/hudStore";
 import { worldState } from "@/lib/worldState";
