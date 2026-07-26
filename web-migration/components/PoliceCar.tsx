f3b3edc702f620eaf1fa4c6e5ba5959a7a6525b8
AkulSharma <sharmaakul2007@gmail.com>
2026-07-27 00:04:13 +0530

    auto: PoliceCar.tsx

--- web-migration/components/PoliceCar.tsx
+++ web-migration/components/PoliceCar.tsx
@@ -69,7 +69,7 @@ export function PoliceCar() {
 
     if (isActive && teleportRequest.pending) {
       teleportRequest.pending = false;
-      body.setTranslation({ x: teleportRequest.x, y: 1, z: teleportRequest.z }, true);
+      body.setTranslation({ x: teleportRequest.x, y: RIDE_HEIGHT, z: teleportRequest.z }, true);
       car.current.h = teleportRequest.h;
       car.current.speed = 0;
       car.current.vLat = 0;
